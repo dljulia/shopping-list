1cb13e6b65508cc712e28b1b213859fee6f5aabe
20ulia <uliatupik88@gmail.com>
2024-05-29 15:33:49 +0300

    Выполненное задание

--- script.js
+++ script.js
@@ -7,3 +7,32 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const sendInput = document.querySelector('#input');
+const taskContainer = document.querySelector('.items');
+
+function createTask() {
+  const taskText = sendInput.value;
+
+  const newTask = document.createElement('div');
+  newTask.classList.add('task');
+  newTask.textContent = taskText;
+
+  if (taskText != '') {
+    taskContainer.prepend(newTask);
+  }
+
+  sendInput.value = '';  
+
+  newTask.addEventListener('click', function() {
+    newTask.classList.toggle('done')
+  });
+}
+
+sendInput.addEventListener('keydown', function(event) {
+  if (event.key == 'Enter') {
+    createTask();
+  }
+}); 
+
+
